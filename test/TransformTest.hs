ec4d7fa81000fa93e7defcd77139c0d2bc81ce88
mihaijulien <mihaijulien@gmail.com>
2025-03-14 18:09:23 +0200

    add more tests

--- test/TransformTest.hs
+++ test/TransformTest.hs
@@ -9,3 +9,21 @@ spec = do
     it "takes an integer as input and outputs the string representation of it" $ do
       num2words 1 `shouldBe` "ichi"
       num2words 91 `shouldBe` "kyuujuuichi"
+
+  describe "separateNumsAndWords" $ do
+    it "separates numbers from words" $ do
+        separateNumsAndWords "123 test" `shouldBe` (123, "test")
+        separateNumsAndWords "321    Test" `shouldBe` (321, "Test")
+
+  describe "assignCounter" $ do
+    it "assign a counter word" $ do
+        assignCounter "chopsticks" `shouldBe` Just "本 (ほん) / hon"
+        assignCounter "books" `shouldBe` Just "冊 (さつ/satsu)"
+        assignCounter "cats" `shouldBe` Just "匹 (ひき) / hiki"
+        assignCounter "horses" `shouldBe` Just "頭 (とう) / tou"
+        assignCounter "students" `shouldBe` Just "人 (にん) / nin"
+
+  describe "transform" $ do
+    it "en -> jp" $ do
+        transform "2 cats" `shouldBe` "ni 匹 (ひき) / hiki"
+        transform "50 cars" `shouldBe` "gojuu 台 (だい) / dai"
