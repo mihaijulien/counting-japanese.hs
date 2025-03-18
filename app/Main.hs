8e6159a61c945f90f8e14b87515480caab0b894d
mihaijulien <mihaijulien@gmail.com>
2025-03-18 21:28:17 +0200

    add TUI with brick library

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,10 @@
 module Main (main) where
 
-import Transform
+--import Transform
+import UI
+import Brick (defaultMain)
 
 main :: IO ()
 main = do
-    putStrLn("Japanese Numbers and Counters")
-    putStrLn("Usage example: \n Input: 2 cats \n Output: ni 二 匹 (ひき) / hiki \n\n")
-    command <- getLine
-    putStrLn $ transform command
+    state <- defaultMain theApp initialState
+    putStrLn "quit"
---
+++ src/UI.hs
@@ -0,0 +1,104 @@
+{-# LANGUAGE TemplateHaskell #-} -- needed for lens
+
+module UI where
+
+import qualified Brick.AttrMap          as A
+import qualified Brick.Widgets.Border   as B
+import qualified Brick.Widgets.Center   as C
+import qualified Brick.Widgets.Edit     as E
+import qualified Brick.Main             as M
+import qualified Brick.Types            as T
+import qualified Graphics.Vty           as V
+
+import Lens.Micro
+import Lens.Micro.TH
+import Lens.Micro.Mtl
+import Brick.Widgets.Core
+import Brick.Util (on)
+
+import Transform
+
+data Name = Edit1 deriving (Ord, Show, Eq)
+data State =
+    State {_editor :: E.Editor String Name
+          ,_definition :: String
+    }
+
+makeLenses ''State
+
+drawUI :: State -> [T.Widget Name]
+drawUI st = [ui]
+    where
+        e1 = E.renderEditor (str . unlines) True (st^.editor)
+        e2 = withAttr definitionAttr $ str (st^.definition)
+
+        countingTable =
+             B.borderWithLabel (str "Counting Japanese") $
+             vLimitPercent 75 $ hLimitPercent 37 $
+             C.center $
+             vBox
+                 [ C.center $ (str "Input: " <+> (hLimit 30 $ vLimit 1 e1))
+                 , C.center $ (hLimit 50 $ e2)
+                 , C.center $ str "In the Japanese language, different counters (e.g., 本 (ほん, hon) for long objects \nor 枚 (まい, mai) for flat objects) are used depending on the item's shape or category.\n"
+                 ]
+
+        instructionsTable =
+             B.borderWithLabel (str "Instructions") $
+             vLimitPercent 75 $ hLimitPercent 37 $
+             C.center $
+             vBox 
+                 [ str "1. Type a numeral followed by a noun"
+                 , str "------------------------------------"
+                 , str "Example input: \"2 cats\""
+                 , str "Example output: \"ni 二 匹 (ひき) / hiki)\""
+                 , str "------------------------------------"
+                 , str "2. Press Enter to see its japanese form"
+                 , str "3. Press ESC to quit the application"
+                 ]
+
+        ui = C.center $ hBox [countingTable, instructionsTable]
+
+
+search :: String -> String
+search word = "Japanese: " ++ transform word
+
+appEvent :: T.BrickEvent Name e -> T.EventM Name State ()
+appEvent e = do
+    editorWidget <- use editor
+    let userInput = unlines (E.getEditContents editorWidget)
+
+    case e of
+        T.VtyEvent vtye -> case vtye of
+            V.EvKey V.KEsc [] -> M.halt
+            V.EvKey V.KEnter [] -> do
+                definition .= search userInput
+                editor .= E.editor Edit1 (Just 1) ""
+                return ()
+            _ -> do
+                zoom editor $ E.handleEditorEvent e
+                return ()
+        _ -> do
+            return ()
+
+definitionAttr :: A.AttrName
+definitionAttr = A.attrName "blackOnWhite"
+
+appStyle :: A.AttrMap
+appStyle = A.attrMap V.defAttr
+    [ (E.editAttr,                    V.red   `on` V.white)
+    , (definitionAttr,                V.black `on` V.yellow)
+    ]
+
+theApp :: M.App State e Name
+theApp =
+    M.App { M.appDraw           = drawUI
+          , M.appChooseCursor   = M.neverShowCursor
+          , M.appHandleEvent    = appEvent
+          , M.appStartEvent     = return ()
+          , M.appAttrMap        = const appStyle
+          }
+
+initialState :: State
+initialState =
+    State (E.editor Edit1 (Just 1) "")
+          ("Japanese form here")
