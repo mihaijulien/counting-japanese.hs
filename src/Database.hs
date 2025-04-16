e76fa846101be360d6c2640e976f3329702e3b20
mihaijulien <mihaijulien@gmail.com>
2025-04-16 23:52:26 +0300

    read db connection setup from .env file

--- src/Database.hs
+++ src/Database.hs
@@ -8,17 +8,25 @@ module Database
 import qualified Database.PostgreSQL.Simple as SQL
 import Database.PostgreSQL.Simple.FromRow
 import Model.JpNumber (JpNumber(JpNumber))
+import System.Environment (getEnv)
+import Configuration.Dotenv (loadFile, defaultConfig)
 
--- TODO: move to .env file
 getConnection :: IO SQL.Connection
-getConnection =
+getConnection = do
+  loadFile defaultConfig
+  host <- getEnv "DB_HOST"
+  database <- getEnv "DB_DATABASE"
+  user <- getEnv "DB_USER"
+  password <- getEnv "DB_PASSWORD"
+  portStr <- getEnv "DB_PORT"
+  let port = read portStr :: Int
   SQL.connect $
     SQL.defaultConnectInfo
-      { SQL.connectHost = "localhost"
-      , SQL.connectDatabase = "japannumbers"
-      , SQL.connectUser = "user"
-      , SQL.connectPassword = "password"
-      , SQL.connectPort = 15432
+      { SQL.connectHost = host
+      , SQL.connectDatabase = database
+      , SQL.connectUser = user
+      , SQL.connectPassword = password
+      , SQL.connectPort = fromIntegral port
       }
 
 instance SQL.FromRow JpNumber where
